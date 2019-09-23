e4a4ff6ab5b0af3b90ab264f8348b9cd034328e1
gnodipac886 <ericdong886@yahoo.com>
2019-09-23 12:06:52 -0500

    update

--- Lab 5/control.sv
+++ Lab 5/control.sv
@@ -77,39 +77,3 @@ module control(input logic clearA_loadB, reset, execute, Clk, bout,
 		endcase
 	end
 endmodule 
-
-
-module multiplier();
-	logic shoutA;
-	logic [8:0] doutA, sumA;
-	logic [7:0] doutB;
-
-	carry_select_adder_9bit adder1(.A(mand), 
-								   .B(), 
-								   .Sum(.sumA), 
-								   .CO());
-
-	shift_reg_9bit regA(
-						.clk(Clk), 
-						.reset(resetA), 
-						.load(loadA), 
-						.shift_en(Shift_en), 
-						.shift_in(), 
-						.din(sumA), 
-						.shift_out(shoutA), 
-						.dout(doutA)
-						); 
-						//what do we do with this empty connection?
-
-	shift_reg_8bit regB(
-						.clk(Clk), 
-						.reset(resetB), 
-						.load(loadB), 
-						.shift_en(Shift_en), 
-						.shift_in(shoutA), 
-						.din(), 
-						.shift_out(bout), 
-						.dout(doutB)
-						); //^^^^^^
-
-endmodule
