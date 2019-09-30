7e0e6bf3d284cb26e348c540fa88cc27c0e1a2f3
gnodipac886 <ericdong886@yahoo.com>
2019-09-30 18:52:10 -0500

    update

--- Lab 6/datapath.sv
+++ Lab 6/datapath.sv
@@ -9,6 +9,26 @@ module datapath (
 					output logic [11:0] LED,
 				);
 
+	//logic SR2;
+	//logic [15:0] sext5out, sext6out, sext9out, sext11out;
+
+	// SEXT5 ext5(.in(IR_out[4:0]), .out(sext5out));
+	// SEXT5 ext6(.in(IR_out[5:0]), .out(sext6out));
+	// SEXT5 ext9(.in(IR_out[8:0]), .out(sext9out));
+	// SEXT5 ext11(.in(IR_out[10:0]), .out(sext11out));
+
+
+
+	// always_comb begin
+	// 	SR2 = IR_out[2:0];
+	// end 
+
+	mux_21_dynamic marmux(.sel(), .d0(), .d1(), .out());
+	mux_21_dynamic addr1mux(.sel(), .d0(), .d1(), .out());
+	mux_41_dynamic pcmux(.sel(), .d0(), .d1(), .d2(), .d3() .out());
+	mux_41_dynamic addr2mux(.sel(), .d0(), .d1(), .d2(), .d3() .out());
+
+	
 
 
 endmodule 
