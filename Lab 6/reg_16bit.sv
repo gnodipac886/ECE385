c6e5f773c37766168503f911a3dcf6fcd10ce6a9
gnodipac886 <ericdong886@yahoo.com>
2019-09-30 19:19:23 -0500

    update

---
+++ Lab 6/reg_16bit.sv
@@ -0,0 +1,13 @@
+module reg_16bit(	
+				input logic clk, reset, load,
+				input logic [15:0] din
+				output logic [15:0] dout
+			  );
+	always_ff @ (posedge clk)
+	begin
+		if(reset)
+			dout <= 16'h0000;
+		else if (load)
+			dout <= din;
+	end
+endmodule
